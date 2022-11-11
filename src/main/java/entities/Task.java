faef6e044a375a3a238ca7f97698194443ba3757
JoAndNight <934569760@qq.com>
2022-11-11 18:21:01 -0500

    Individual work: Created classes for use case Presenters. Implemented task entity class and corresponding testing file. Group work during meeting: Implemented Factory.java.

--- src/main/java/entities/Task.java
+++ src/main/java/entities/Task.java
@@ -1,23 +1,59 @@
 package entities;
 
-import java.util.Date;
+import java.util.Calendar;
 import java.util.Iterator;
 
-public class Task implements Iterable{
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
+public class Task{
     public String name;
-    private boolean complete;
-    private boolean reminders;
-    private boolean visibility;
-    private String colour;
-    private Category taskCategory;
-    private Date deadline;
+    public boolean completeStatus;
+    public boolean reminders;
+    public boolean visibility;
+    public Category taskCategory;
+    public Calendar deadline;
 
-    public Task(){
+    public Task(String name, boolean reminders, Calendar deadline){
+        // @param The name of the task.
+        // @param Whether the reminder is turned on. (Remind the user 24h before the deadline)
+        // @param The colour for the task. User can choose from 7 given colors. (Red, Orange, Yellow, Green, Blue, Purple, Pink)
+        // @param The deadline of the task.
+
+        this.name = name;
+        this.completeStatus = FALSE;
+        this.reminders = reminders;
+        this.visibility = TRUE;
+        this.deadline = deadline;
+    }
+
+    public void setName(String name){
+        this.name = name;
 
     }
 
-    @Override
-    public Iterator iterator() {
-        return null;
+    public void markAsComplete(){
+        this.completeStatus = TRUE;
     }
+    public void markAsIncomplete(){ this.completeStatus = FALSE;}
+
+    public void setReminders(boolean remindersStatus){
+        // Set whether there will be reminders for the task.
+        this.reminders = remindersStatus;
+    }
+
+    public void setVisibility(boolean visible){
+        // Set the visibility of the task. If TRUE, then the task is visible in both to-do list view and calendar view,
+        // otherwise the task will be invisible.
+        this.visibility = visible;
+    }
+
+    public void setTaskCategory(Category taskCategory) {
+        this.taskCategory = taskCategory;
+    }
+
+    public void setDeadline(Calendar deadline) {
+        this.deadline = deadline;
+    }
+
 }
--- src/main/java/entities/TaskFactory.java
+++ src/main/java/entities/TaskFactory.java
@@ -2,9 +2,15 @@ package entities;
 
 import java.util.HashMap;
 
-public class TaskFactory extends Factory{
+public class TaskFactory extends Factory<Task>{
+    HashMap<Integer, Task> h_items;
 
     public TaskFactory(){
+        super();
+    }
+
+    @Override
+    public void addItem(Task item) {
 
     }
 }
