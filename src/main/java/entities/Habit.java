11938a4243c6a0d1c717be546e5cc62e8f848433
Mugdha Banthwan <mugdha.banthwan@mail.utoronto.ca>
2022-11-11 18:19:30 -0500

    Code for creating and tracking a habit.

--- src/main/java/entities/Habit.java
+++ src/main/java/entities/Habit.java
@@ -1,5 +1,6 @@
 package entities;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 
 public class Habit {
@@ -7,7 +8,42 @@ public class Habit {
     private String name;
     private String type;
     private boolean reminderTrue;
-    private String color;
-    private HashMap completionMap;
+
+    private int frequency;
+    private HashMap<String, Integer> frequencyMap;
+
+    public Habit(String title, String t, boolean value ){
+
+        this.name = title;
+        this.type = t;
+        this.reminderTrue = value;
+        this.frequency = 0;
+        this.frequencyMap = new HashMap<>();
+        this.frequencyMap.put(LocalDate.now().toString(), frequency);
+    }
+
+    public void setName(String title) {this.name = title;}
+
+    public void setType(String t) {this.type = t;}
+
+    public void setReminder(boolean v) {this.reminderTrue= v;}
+
+    public String getName() { return this.name;}
+
+    public String getType() { return this.type;}
+
+    public boolean getReminder() {return this.reminderTrue;}
+
+
+    public int getFrequency() { return this.frequency;}
+
+    public HashMap<String, Integer> getFrequencyMap() { return this.frequencyMap;}
+
+    public void markFrequency(){
+        this.frequency += 1;
+        this.frequencyMap.put(this.name, this.frequency);
+    }
 
 }
+
+
