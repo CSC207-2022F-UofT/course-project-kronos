4e2f1ad5c4d6a8a2e6b3af4d0e95d12fa195cc2b
emily <emilydu@live.ca>
2022-11-11 18:08:00 -0500

    Finished the Category Entity and Category Use Case files.

--- src/main/java/task_use_cases/editCategoryVisibility.java
+++ src/main/java/task_use_cases/editCategoryVisibility.java
@@ -1,5 +1,11 @@
 package task_use_cases;
 
+import entities.Category;
+
 public class editCategoryVisibility {
     // Acts like a filter.
+
+    public void editCategoryVisibility(Category category, Boolean state) {
+        category.setVisibility(state);
+    }
 }
