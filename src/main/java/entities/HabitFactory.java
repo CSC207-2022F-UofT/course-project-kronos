30210fa025133647df23dfa65b05e6e960cc65e0
Mugdha Banthwan <mugdha.banthwan@mail.utoronto.ca>
2022-11-12 00:01:50 -0500

    Factory for Habit entity.

--- src/main/java/entities/HabitFactory.java
+++ src/main/java/entities/HabitFactory.java
@@ -2,8 +2,28 @@ package entities;
 
 import java.util.HashMap;
 
-public class HabitFactory extends Factory{
-    public HabitFactory(){
+public class HabitFactory extends Factory<Habit>{
+
+    public HashMap<String, Habit> h_items;
+
+    public HabitFactory(){}
+
+    @Override
+    public void addItem(Habit item) {
+        h_items.put(item.getName(), item);
+    }
+
+    @Override
+    public void removeItem(Habit item) {
+        h_items.remove(item.getName(), item);
+    }
+
+    public void updateKey(String oldKey, String newKey){
+        Habit value = h_items.get(oldKey);
+        removeItem(value);
+        value.setName(newKey);
+        addItem(value);
 
     }
 }
+
