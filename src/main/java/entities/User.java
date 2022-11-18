e5b6792b1d62fee38bbec076425a1adb1f4e2415
happynasit <happynasit@gmail.com>
2022-11-18 17:49:49 -0500

    Added habit factory, category factory, tasks and timer to the User class

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -40,6 +40,8 @@ public class User {
         this.tomato = timer;
     }
 
+    public User() {}
+
     // GETTER METHODS
 
     public String getPassword(){
---
+++ src/main/java/userui/MenuPage.java
@@ -0,0 +1,4 @@
+package userui;
+
+public class MenuPage {
+}
