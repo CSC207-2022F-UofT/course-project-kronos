a3f1a8c997e796ddc4b1b924727929fcbff878dc
Mugdha Banthwan <mugdha.banthwan@mail.utoronto.ca>
2022-11-19 23:18:59 -0500

    Habit_use_case

--- src/main/java/Habit_use_case/CreateHabit.java
+++ src/main/java/Habit_use_case/CreateHabit.java
@@ -6,10 +6,10 @@ public class CreateHabit {
 
     /**
      * The constructor of Order.
+     * @param u -  the user of the habit
      * @param title - the name of the habit that is to be created
      * @param t -  the type of the habit
      * @param v -  State of the reminder of the habit.
-     * @param u -  the user of the habit
      */
     public static void createHabit(User u, String title, String t, boolean v) {
         Habit h = new Habit(title, t, v);
