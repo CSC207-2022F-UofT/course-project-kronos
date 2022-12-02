0c1005549e6d7b50dbb81a4859fb031132199b8e
JoAndNight <934569760@qq.com>
2022-12-01 22:28:54 -0500

    Add JavaDoc to Presenters and Exceptions

--- src/main/java/controllers/tasks/MarkVisibilityPresenter.java
+++ src/main/java/controllers/tasks/MarkVisibilityPresenter.java
@@ -3,8 +3,16 @@ package controllers.tasks;
 import use_cases.tasks.mark_task_visibility.MarkVisibilityOutputBound;
 import use_cases.tasks.mark_task_visibility.MarkVisibilityOutputData;
 
+/**
+ * -- Interface Adaptor Layer --
+ * The Presenter that passes output data from the use case of MarkVisibility (for task) to the UI.
+ */
 public class MarkVisibilityPresenter implements MarkVisibilityOutputBound{
-
+    /**
+     * This method is called when the marking the Visibility of task is successful. It prepares view.
+     * @param outputData - the output data.
+     * @return the output data.
+     */
     @Override
     public MarkVisibilityOutputData prepareSuccessView(MarkVisibilityOutputData outputData) {
         return outputData;
