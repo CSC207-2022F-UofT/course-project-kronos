8194c0fe3a4fa8661753cf8eba715d58bfc66a07
emily <emilydu@live.ca>
2022-12-02 18:34:47 -0500

    started UI files

--- src/main/java/ui/category/ToDoScreen.java
+++ src/main/java/ui/category/ToDoScreen.java
@@ -1,9 +1,14 @@
 package ui.category;
 
+import entities.Category;
+import entities.CategoryCollection;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+// have to use mark_task_completion
 
 /**
  * -- User Interface Layer --
@@ -14,22 +19,99 @@ import java.awt.event.ActionListener;
 public class ToDoScreen extends JFrame implements ActionListener {
 
     private JFrame toDoFrame;
-    private JButton newTask;
+    private CategoryCollection categories;
+    private JPanel header;
+    private JScrollPane body;
+    private static JLabel title;
     private JButton newCategory;
+    private JButton menu;
 
-    public ToDoScreen() {
-        toDoFrame = new JFrame ("To Do List");
+    public ToDoScreen(CategoryCollection categories) {
+        this.categories = categories;
+        toDoFrame = new JFrame("To Do List");
         toDoFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         toDoFrame.setSize(1000, 800);
-        toDoFrame.setLayout(new GridBagLayout());
+        toDoFrame.setLayout(new BorderLayout());
+
+        header = new JPanel();
+        header.setLayout(new GridBagLayout());
+
+        body = new JScrollPane();
+        body.setLayout(new GridBagLayout());
+
+        toDoFrame.add(header, BorderLayout.NORTH);
+        toDoFrame.add(body, BorderLayout.CENTER);
+
         GridBagConstraints c = new GridBagConstraints();
+
+        /**
+         * Creating and setting the placement of the title
+         */
+        title = new JLabel("To Do List");
+        c.gridwidth = 3;
+        c.ipady = 20;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        header.add(title, c);
+
+        /**
+         * Creating and setting the placement of the "create" buttons and menu button
+         */
+
+        newCategory = new JButton("New Category");
+        c.ipady = 1;
+        c.gridx = 2;
+        c.gridy = 0;
+        header.add(newCategory, c);
+
+        menu = new JButton("Menu");
+        c.ipady = 1;
+        c.gridx = 0;
+        c.gridy = 0;
+        header.add(menu, c);
+
+        newCategory = new JButton("New Category");
+
+        /**
+         * Adding table headlining row for tasks
+         */
+        JLabel taskName = new JLabel("Task Name");
+        c.ipady = 2;
+        c.gridx = 0;
+        c.gridy = 0;
+        header.add(taskName, c);
+
+        JLabel deadline = new JLabel("Deadline");
+        c.ipady = 2;
+        c.gridx = 1;
+        c.gridy = 0;
+        header.add(deadline, c);
+
+        JLabel completion = new JLabel("Completion");
+        c.ipady = 2;
+        c.gridx = 2;
+        c.gridy = 0;
+        header.add(completion, c);
+
+
 
     }
 
     /**
-     * Updates the to do list to reflect the database
+     * Updates the to do list to reflect the database's information
      */
     public void updateList() {
+        // loop through all the categories
+        // for each category, loop through the task collection
+        // update the body panel so that the name of the category (column 0) and add a button to the right most column for newTask button
+
+        int y = 3;
+        for (Integer id: categories.categories.keySet()) {
+            Category cat = categories.getItem(id);
+            // new constraints for gridlayout
+
+        }
 
     }
 
