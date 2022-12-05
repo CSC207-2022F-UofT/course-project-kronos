2b260695eccde2cafe3778f0d520133643aa23c3
m-banthwan <113299553+m-banthwan@users.noreply.github.com>
2022-12-04 21:14:31 -0500

    Add files via upload

---
+++ src/main/java/controllers/habits/delete/DeleteHabitController.java
@@ -0,0 +1,30 @@
+package controllers.habits.delete;
+
+import use_cases.habits.delete_habit.DeleteHabitInputBoundary;
+import use_cases.habits.delete_habit.DeleteHabitInputData;
+import use_cases.habits.delete_habit.DeleteHabitOutputData;
+
+/**
+ Controller class for deleting a habit.
+ */
+public class DeleteHabitController {
+
+    final DeleteHabitInputBoundary userInput;
+
+    /**
+     * Constructor for this class.
+     * @param habitGateway - The input from the user.
+     */
+    public DeleteHabitController(DeleteHabitInputBoundary habitGateway) {
+        this.userInput = habitGateway;
+    }
+
+    /**
+     * @param habitID - The name (ID) of the habit to be deleted.
+     * @return returns the output data after deletion.
+     */
+    DeleteHabitOutputData delete(String habitID){
+        DeleteHabitInputData inputData = new DeleteHabitInputData(habitID);
+        return userInput.delete(inputData);
+    }
+}
