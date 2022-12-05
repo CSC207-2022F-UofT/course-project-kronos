1be635aa7ba54fb0fa696575c89d48bba93a154e
AngelEyesMagicMaker <htongyuky.wang@mail.utoronto.ca>
2022-12-05 18:08:27 -0500

    Add Timer use cases changes.

---
+++ src/main/java/use_cases/timer_use_cases/create_single_timer/CreateWorkTimerInputBoundary.java
@@ -0,0 +1,4 @@
+package use_cases.timer_use_cases.create_single_timer;
+
+public class CreateWorkTimerInputBoundary {
+}
---
+++ src/main/java/use_cases/timer_use_cases/edit_timer/EditTimer.java
@@ -0,0 +1,28 @@
+package use_cases.timer_use_cases.edit_timer;
+
+/**
+ * Edit properties of the whole timer. Properties include: work timer minutes and rest timer minutes.
+ */
+
+public class EditTimer {
+
+    private final EditTimerOutputBoundary outputBoundary;
+
+    public EditTimer(EditTimerOutputBoundary outputBoundary) {
+        this.outputBoundary = outputBoundary;
+    }
+
+    /**
+     * Edit the properties of a timer. The edible properties are work timer minutes and rest timer minutes for a
+     * timer object.
+     *
+     * @param inputData - the input data which contain an input work timer minutes and an input rest timer minutes.
+     * @return the output data after editing.
+     */
+
+    public EditTimerOutputData edit(EditTimerInputData inputData) {
+        //If one of the input is empty or containing things not integer.
+        EditTimerOutputData outputData = new EditTimerOutputData();
+        return outputBoundary.prepareFailView(outputData);
+    }
+}
