5406983798b9c3c1db3586ff7985fb7cde6a992b
AngelEyesMagicMaker <htongyuky.wang@mail.utoronto.ca>
2022-12-05 17:57:04 -0500

    Add TimerTomato changes.

--- src/main/java/entities/TimerTomato.java
+++ src/main/java/entities/TimerTomato.java
@@ -1,24 +1,93 @@
 package entities;
 
 import javax.swing.Timer;
-import java.io.Serializable;
-
-public class TimerTomato implements Serializable {
+public class TimerTomato {
 
 
     private static String restTime;
-
-
     private static String workTime;
     public static int timerCycle;
     private String work_timeLeft;
     private String rest_timeLeft;
-    private Timer timer;
+    private Timer workTimer;
+    private Timer restTimer;
 
-    public TimerTomato(String workTime, String restTime, int timerCycle) {
-        this.workTime = workTime;
-        this.restTime = restTime;
-        this.timerCycle = timerCycle;
+    public TimerTomato(){}
+
+    public TimerTomato(String workTime, String restTime, int timerCycle){
+        TimerTomato.workTime = workTime;
+        TimerTomato.restTime = restTime;
+        TimerTomato.timerCycle = timerCycle;
+
     }
-}
 
+
+    /**
+     * Set the minutes of the work Timer.
+     * @param workTime - The minutes needs for the work timer.
+     */
+
+    public static void setWorkTime(String workTime) {
+        TimerTomato.workTime = workTime;
+    }
+
+    /**
+     * Set the minutes of the rest Timer.
+     * @param restTime - The minutes needs for the rest timer.
+     */
+
+    public static void setRestTime(String restTime) {
+        TimerTomato.restTime = restTime;
+    }
+
+    /**
+     * Set the number of cycle needed for the loop of workTimer and restTimer.
+     * @param timerCycle - The cycle needs for the work timer.
+     */
+
+    public void setTimerCycle(int timerCycle) {
+        TimerTomato.timerCycle = timerCycle;
+    }
+
+    public static String getWorkTime() {
+        return workTime;
+    }
+
+    public static String getRestTime(){
+        return restTime;
+    }
+
+    public static int getTimerCycle() {
+        return timerCycle;
+    }
+
+    public String getWork_timeLeft() {
+        return work_timeLeft;
+    }
+
+    public String getRest_timeLeft() {
+        return rest_timeLeft;
+    }
+
+
+
+    public int getTimeStatus(Timer timer){
+        // timerState:
+        // value 0 - stop
+        // value 1 - on
+        // value 2 - pause
+        int timerState;
+        if (timer.isRunning()){
+            timerState = 1;
+        }
+        else if (!timer.isRunning()) {
+            timerState = 0;
+        }
+        else  {
+            timerState = 2;
+        }
+        return timerState;
+    }
+
+
+}
