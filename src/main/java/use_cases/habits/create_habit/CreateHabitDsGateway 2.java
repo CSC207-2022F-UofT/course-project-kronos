ccfdc989fab21b1867d3cb9a5d2fe0b747d877b8
happynasit <happynasit@gmail.com>
2022-12-08 13:32:48 -0500

    Deletes unnecessary Habits classes

--- src/main/java/use_cases/habits/create_habit/CreateHabitDsGateway 2.java
+++
@@ -1,9 +0,0 @@
-package use_cases.habits.create_habit;
-
-/**
- * Gateway for creating a habit.
- */
-public interface CreateHabitDsGateway {
-
-    void save(CreateHabitDsRequestModel requestModel);
-}
