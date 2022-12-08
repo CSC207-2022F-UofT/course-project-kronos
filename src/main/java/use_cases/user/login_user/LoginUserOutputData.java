03809bed912cc85aabe9fec0af57683672917c04
happynasit <happynasit@gmail.com>
2022-12-08 02:46:37 -0500

    Changes in LoginUserOutputData

--- src/main/java/use_cases/user/login_user/LoginUserOutputData.java
+++ src/main/java/use_cases/user/login_user/LoginUserOutputData.java
@@ -4,8 +4,10 @@ import entities.CommonUser;
 
 /**
  *  This class is a container for the output data related to user login.
+ * @author happynasit
  */
 public class LoginUserOutputData {
+    boolean isLoginSuccessful;
     private final String message;
     private final CommonUser user;
     private final String emailAddress;
@@ -14,25 +16,32 @@ public class LoginUserOutputData {
     /**
      * A constructor that is used when there is a failure to log into the account.
      * @param message - the failure message.
-     * @param emailAddress - the email address of the user
+     * @param isLoginSuccessful - the boolean value to show if the user is logged in or not
+     */
+    public LoginUserOutputData(boolean isLoginSuccessful, String message ) {
+        this.isLoginSuccessful = isLoginSuccessful;
+        this.message = message;
+        this.emailAddress = null;
+        this.user = null;
+    }
+
+    /**
+     * A constructor that is used when there is NO failure to log into the account.
+     * @param isLoginSuccessful - the boolean value to show if the user is logged in or not
+     * @param emailAddress - the emailAddress of the user
      * @param user - the User object of the user.
      */
-    public LoginUserOutputData(String message, String emailAddress, CommonUser user) {
-        this.message = message;
+    public LoginUserOutputData(boolean isLoginSuccessful, String emailAddress, CommonUser user) {
+        this.isLoginSuccessful = isLoginSuccessful;
+        this.message = null;
         this.emailAddress = emailAddress;
         this.user = user;
     }
 
     /**
-     * A constructor that is used when there is NO failure to log into the account.
-     * @param emailAddress - the emailAddress of the user
-     * @param user - the User object of the user.
+     * @return if the login is successful or not
      */
-    public LoginUserOutputData(String emailAddress, CommonUser user) {
-        this.message = null;
-        this.emailAddress = emailAddress;
-        this.user = user;
-    }
+    public boolean getIsLoginSuccessful(){return isLoginSuccessful;}
 
     /**
      * @return the message based on the output
