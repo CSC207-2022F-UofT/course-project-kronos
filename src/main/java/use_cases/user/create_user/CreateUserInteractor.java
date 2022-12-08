11049b25e57a6cae0d70306e114fc710e797f8e7
happynasit <happynasit@gmail.com>
2022-12-08 02:33:58 -0500

    Changes in CreateUserInteractor

--- src/main/java/use_cases/user/create_user/CreateUserInteractor.java
+++ src/main/java/use_cases/user/create_user/CreateUserInteractor.java
@@ -4,20 +4,16 @@ import entities.CommonUser;
 
 /**
  * The Interactor that is responsible for creating a new user.
+ * @author happynasit
  */
 public class CreateUserInteractor implements CreateUserInputBoundary{
-
-    private final CreateUserOutputBoundary outputBoundary;
     private final CreateUserDsGateway gateway;
 
     /**
      * Constructor method for CreateUserInteractor
-     *
-     * @param outputBoundary obtained
      * @param gateway of the user
      */
-    public CreateUserInteractor(CreateUserOutputBoundary outputBoundary, CreateUserDsGateway gateway) {
-        this.outputBoundary = outputBoundary;
+    public CreateUserInteractor(CreateUserDsGateway gateway) {
         this.gateway = gateway;
     }
 
@@ -29,21 +25,16 @@ public class CreateUserInteractor implements CreateUserInputBoundary{
 
     @Override
     public CreateUserOutputData create(CreateUserInputData inputData) {
-        if (inputData.getEmailAddress().equals("") || inputData.getFirstName().equals("") || inputData.getLastName().equals("")
-        || inputData.getPassword().equals("") || inputData.getRepeatPassword().equals("")){
-            CreateUserOutputData outputData = new CreateUserOutputData("Invalid parameters");
-            return outputBoundary.prepareFailView(outputData);
-        }else if (gateway.userExistsByEmail(inputData.getEmailAddress())){
-            CreateUserOutputData outputData = new CreateUserOutputData("User email already exists. " +
-                    "Please enter a new email address.");
-            return outputBoundary.prepareFailView(outputData);
-        }else {
-            // case when the repeated password does not match the password
-            if (!inputData.getPassword().equals(inputData.getRepeatPassword())) {
-                CreateUserOutputData outputData = new CreateUserOutputData("Both passwords do not match. " +
-                        "Please re enter the password.");
-                return outputBoundary.prepareFailView(outputData);
-            }
+        if (inputData.getEmailAddress().equals("") || inputData.getPassword().equals("")
+                || inputData.getRepeatPassword().equals("") || inputData.getFirstName().equals("") || inputData.getLastName().equals("")){
+            return new CreateUserOutputData(false, "Empty parameters. Try again!");
+        }
+        if (gateway.userExistsByEmail(inputData.getEmailAddress())){
+            return new CreateUserOutputData(false, "Username already exists!");
+        }
+        // case when the repeated password does not match the password
+        if (!inputData.getPassword().equals(inputData.getRepeatPassword())) {
+            return new CreateUserOutputData(false, "Both passwords do not match");
         }
 
         CommonUser user = new CommonUser(inputData.getFirstName(), inputData.getLastName(),
@@ -51,9 +42,7 @@ public class CreateUserInteractor implements CreateUserInputBoundary{
 
         if (!user.passwordStrength()){
             // if the given password is not strong enough then it shows fail view
-            CreateUserOutputData outputData = new CreateUserOutputData("Password is not strong enough " +
-                    "Please re enter the password.");
-            return outputBoundary.prepareFailView(outputData);
+            return new CreateUserOutputData(false, "The password is not strong enough");
 
         }else{
 
@@ -61,18 +50,10 @@ public class CreateUserInteractor implements CreateUserInputBoundary{
             // it creates the database request model and the gateway saves the email along with the user object
             CreateUserDsRequestModel userDsRequestModel = new CreateUserDsRequestModel(inputData.getEmailAddress(), user);
             gateway.save(userDsRequestModel);
-            CreateUserOutputData outputData = new CreateUserOutputData(user);
-            return outputBoundary.prepareSuccessView(outputData);
+            return new CreateUserOutputData(true, user);
         }
     }
 
-    /**
-     *
-     * @return the success view or the fail view
-     */
-    public CreateUserOutputBoundary getOutputBoundary() {
-        return outputBoundary;
-    }
 
     /**
      * @return the database gateway
