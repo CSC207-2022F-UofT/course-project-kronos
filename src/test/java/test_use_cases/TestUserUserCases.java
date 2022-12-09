4988eb96cf113d0e53db176b0d8258cd6a79cc2a
happynasit <happynasit@gmail.com>
2022-12-08 19:44:56 -0500

    Made changes with test cases

--- src/test/java/test_use_cases/TestUserUserCases.java
+++ src/test/java/test_use_cases/TestUserUserCases.java
@@ -3,6 +3,8 @@ package test_use_cases;
 import database.DatabaseUser;
 import org.junit.jupiter.api.Test;
 import use_cases.user.create_user.*;
+import use_cases.user.login_user.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestUserUserCases {
@@ -23,6 +25,7 @@ public class TestUserUserCases {
 
             @Override
             public CreateUserOutputData prepareFailView(CreateUserOutputData outputData){
+                assertFalse(outputData.getIsSuccessful());
                 return null;
             }
         };
@@ -33,6 +36,36 @@ public class TestUserUserCases {
                 "test", "testagain", "Tester1234");
 
         CreateUserOutputData outputData = inputBoundary.create(inputData);
+        outputBoundary.prepareFailView(outputData);
 
     }
+
+    @Test()
+    public void login() {
+        LoginUserOutputBoundary outputBoundary = new LoginUserOutputBoundary() {
+            @Override
+            public LoginUserOutputData prepareSuccessView(LoginUserOutputData outputData){
+                assertEquals("tester@gmail.com", outputData.getUser().getEmailAddress());
+                assertNull(outputData.getMessage()); // any creation time is fine.
+                assertTrue(outputData.getIsLoginSuccessful());
+                return null;
+            }
+
+            @Override
+            public LoginUserOutputData prepareFailView(LoginUserOutputData outputData){
+                assertFalse(outputData.getIsLoginSuccessful());
+                return null;
+            }
+        };
+        LoginUserDsGateway databaseUser = new DatabaseUser("data.ser");
+        DatabaseUser databaseUser1 = new DatabaseUser("data.ser");
+
+        LoginUserInputBoundary inputBoundary = new LoginUserInteractor(databaseUser1, databaseUser);
+        LoginUserInputData inputData = new LoginUserInputData("tester@gmail.com", "Tester1234");
+
+        LoginUserOutputData outputData = inputBoundary.login(inputData);
+        outputBoundary.prepareSuccessView(outputData);
+
+    }
+
 }
